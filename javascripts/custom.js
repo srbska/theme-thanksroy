20c98efd07a46a925da5daae1f702cf54796170a
edsilv <e.silverton@gmail.com>
2017-01-15 23:39:05 +0000

    flexgrid

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -9,8 +9,14 @@
         var maxItemWidth = 280;
         var minItemWidth = 80;
 
-        $items.each(function(index, item) {
+        // if there are fewer totalItems than maxItemsPerRow
+        // make maxItemsPerRow = totalItems
 
+        if (totalItems < maxItemsPerRow) {
+            maxItemsPerRow = totalItems;
+        }
+
+        $items.each(function(index, item) {
             var $item = $(item);           
             var parentWidth = $item.parent().width();
             var itemWidth = Math.floor(Math.min(maxItemWidth, parentWidth / maxItemsPerRow));
@@ -30,6 +36,21 @@
             var $img = $item.find('img');
             $img.height(itemWidth - $img.verticalPadding());
         });
+
+        // don't allow items to be below this width.
+        //var minItemWidth = 100;
+
+        // items per row (n) = how many items can fit at maxWidth
+        // for every nth item, add a 'wrap' class.
+
+        // $('.item.record').each(function(index, item) {
+        //     // make it half width of parent
+        //     var $item = $(item);
+        //     var width = $item.parent().width() / 2;
+        //     var $a = $item.find('a.image');
+        //     var margins = parseInt($a.css('marginLeft')) + parseInt($a.css('marginRight'));
+        //     $a.height(width - margins);
+        // });
     }
 
     $(function() {
