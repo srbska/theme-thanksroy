3db926e1a30044e2f73da09995d0e1c5a8614ec4
edsilv <e.silverton@gmail.com>
2017-08-28 14:13:06 +0100

    comment

--- index.php
+++ index.php
@@ -45,17 +45,17 @@
                     
                 }
 
-                // if the width is lower than 720 (houses don't fit)
-                // if (win_w < 800) {
-                //     $bg.css({height: 'auto', width: (win_w * 2) - 100 + 'px'});
-                // } else {
-                //     // Determine whether width or height should be 100%
-                //     if ((win_w / win_h) < ($bg.width() / $bg.height())) {
-                //         $bg.css({height: '100%', width: 'auto'});
-                //     } else {
-                //         $bg.css({width: '100%', height: 'auto'});
-                //     }
-                // }
+                // if the width is lower than 800 (houses don't fit)
+                //if (win_w < 800) {
+                    $bg.css({height: 'auto', width: (win_w * 2) - 100 + 'px'});
+                //} else {
+                    // Determine whether width or height should be 100%
+                    if ((win_w / win_h) < ($bg.width() / $bg.height())) {
+                        $bg.css({height: '100%', width: 'auto'});
+                    } else {
+                        $bg.css({width: '100%', height: 'auto'});
+                    }
+                //}
 
                 $('#debug').text(win_w);
 
