09082f728dbd5f6692a89fce56f9475d4f42e8f3
edsilv <e.silverton@gmail.com>
2017-10-07 13:34:37 +0100

    auto

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -6,7 +6,7 @@
         var totalItems = $items.length;
 
         var maxItemsPerRow = 4;
-        var maxItemWidth;
+        var maxItemWidth = 300;
         var minItemWidth = 80;
 
         // if there are fewer totalItems than maxItemsPerRow
@@ -21,7 +21,6 @@
         $items.each(function(index, item) {
             var $item = $(item);
             var $img = $item.find('img');
-            maxItemWidth = $img.width();
             var parentWidth = $item.parent().width();
 
             // the item width is either the maxItemWidth, or the parent width divided by the max number
