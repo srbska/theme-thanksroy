9b9b9a6d6b4022e0fabd4349c93e01598fd670f2
edsilv <e.silverton@gmail.com>
2017-10-07 13:30:30 +0100

    auto

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -22,23 +22,27 @@
             var autoWidth = false;
             var $item = $(item);
             var $img = $item.find('img');
-            maxItemWidth = $img.outerWidth();
+            maxItemWidth = $img.width();
             var parentWidth = $item.parent().width();
 
             // the item width is either the maxItemWidth, or the parent width divided by the max number
-            // of items per row. whichever is smaller.
+            // of items per row. whichever is smaller. we don't want the images scaling beyond their max width.
             var itemWidth = Math.floor(Math.min(maxItemWidth, parentWidth / maxItemsPerRow));
 
             //var itemWidth = Math.floor(parentWidth / maxItemsPerRow);
 
-            itemWidth -= Math.floor($item.horizontalPadding());
+            //itemWidth -= Math.floor($item.horizontalPadding());
 
             if (itemWidth < minItemWidth) {
                 autoWidth = true;
                 itemWidth = 'auto';
                 $item.addClass('autoWidth');
+                $img.height('auto');
+                $img.width('100%');
             } else {
                 $item.removeClass('autoWidth');
+                $img.height(itemWidth - $img.verticalPadding());
+                $img.width('auto');
             }
 
             $item.width(itemWidth);
@@ -46,14 +50,6 @@
             // if ((index + 1) % maxItemsPerRow === 0) {
             //     $item.addClass('wrap');
             // }
-
-            if (!autoWidth) {                
-                $img.height(itemWidth - $img.verticalPadding());
-                $img.width('auto');
-            } else {
-                $img.height('auto');
-                $img.width('100%');
-            }
             
         });
 
